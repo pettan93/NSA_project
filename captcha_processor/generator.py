8f83ccd310f6454f909fd8b0b653b377b2de28a4
Pettan93 <panhammer@seznam.cz>
2017-04-05 13:28:20 +0200

    Alphabet generator, generated 100 images for each character in lowercase and uppercase variant

--- captcha_processor/generator.py
+++ captcha_processor/generator.py
@@ -1,9 +1,11 @@
+import os
+import string
 from subprocess import check_output
 
 RESOURCES_DIR_PATH = '../resources/'
 
 
-def generate_captcha(text, output_name, settings_dict=None):
+def generate_captcha(text, output_path, settings_dict=None):
     settings_args = ["height",  # default 60
                      "width",  # default (number of text characters * 60)
                      "fontSize",  # default 60
@@ -23,7 +25,7 @@ def generate_captcha(text, output_name, settings_dict=None):
         "java -jar "
         + RESOURCES_DIR_PATH + "captcha_generator.jar "
         + text + " "
-        + RESOURCES_DIR_PATH + "/output/" + output_name + " "
+        + output_path + " "
         + RESOURCES_DIR_PATH + "fonts/boxpot.ttf "
         + settings_dict["height"] + " "
         + settings_dict["width"] + " "
@@ -36,5 +38,25 @@ def generate_captcha(text, output_name, settings_dict=None):
 
 
 # usages
-generate_captcha("foo", "out1.png", {"showGrid": "true", "fontSize": "200"})
-generate_captcha("bar", "out2.png")
+# generate_captcha("foo", "out1.png", {"showGrid": "true", "fontSize": "200"})
+# generate_captcha("bar", "out2.png")
+
+
+LETTER_SAMPLES = 100
+
+for index, char in enumerate(string.ascii_lowercase):
+    print("Generating letter '" + char + "' [" + str(index + 1) + "/" + str(len(string.ascii_lowercase)) + "]")
+
+    cap_char = char.capitalize()
+
+    lowercase_dir = RESOURCES_DIR_PATH + "output/alphabet/lowercase/" + char
+    os.makedirs(lowercase_dir)
+    for i in range(LETTER_SAMPLES):
+        print(" - lowercase image [" + str(i + 1) + "/" + str(LETTER_SAMPLES) + "]")
+        generate_captcha(char, lowercase_dir + "/" + str(i + 1) + ".png", {"height": "100", "width": "100"})
+
+    uppercase_dir = RESOURCES_DIR_PATH + "output/alphabet/uppercase/" + cap_char
+    os.makedirs(uppercase_dir)
+    for i in range(LETTER_SAMPLES):
+        print(" - uppercase image [" + str(i + 1) + "/" + str(LETTER_SAMPLES) + "]")
+        generate_captcha(cap_char, uppercase_dir + "/" + str(i + 1) + ".png", {"height": "100", "width": "100"})

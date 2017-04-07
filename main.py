14c87dac7616802d29ce5df4d0b9e455af3b3e35
Hnatekmar <m.hnatma@gmail.com>
2017-04-07 20:28:29 +0200

    Menší úprava neuronky

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ import os
 import random
 import matplotlib.pyplot as plt
 
+class Batcher:
+    def __init__(self, arr):
+        self.array = arr
+
+    def next_batch(self, size):
+        random.shuffle(self.array)
+        return self.array[:size]
 
 def sigmoid(x):
     x = np.clip(x, -500, 500)
@@ -22,7 +29,7 @@ class NeuralNetwork:
     """
     def __init__(self, input_size, output_size):
         self.hidden_layer = tf.Variable(tf.zeros([input_size, output_size]))
-        self.b = b = tf.Variable(tf.zeros([output_size]))
+        self.b = tf.Variable(tf.zeros([output_size]))
         self.input_size = input_size
         self.output_size = output_size
         self.session = tf.InteractiveSession()
@@ -34,10 +41,8 @@ class NeuralNetwork:
         :return: predikce
         """
         x = tf.placeholder(tf.float32, [None, self.input_size])
-        feed_forward = tf.nn.softmax(tf.matmul(x, self.hidden_layer) + self.b)
-        tf.global_variables_initializer().run()
-        return np.argmax(self.session.run(feed_forward, feed_dict = {x: input_data})[0])
-
+        feed_forward = tf.argmax(tf.nn.softmax(tf.matmul(x, self.hidden_layer) + self.b), 1)
+        return self.session.run(feed_forward, feed_dict={x: input_data})
 
     def train(self, training_set, learning_rate, epochs):
         x = tf.placeholder(tf.float32, [None, self.input_size])
@@ -46,9 +51,10 @@ class NeuralNetwork:
         cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(feed_forward), reduction_indices=[1]))
         train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
         tf.global_variables_initializer().run()
+        batcher = Batcher(list(zip(training_set[0], training_set[1])))
         for i in range(epochs):
-            self.session.run(train_step, feed_dict = {x: training_set[0], y_: training_set[1]})
-
+            training, expected = list(zip(*batcher.next_batch(500)))
+            self.session.run(train_step, feed_dict={x: training, y_: expected})
 
 
 def image_to_vector(path):
@@ -57,6 +63,7 @@ def image_to_vector(path):
     maximum = max(data) 
     data = [x / maximum for x in data]
     return data
+
 
 def one_hot(x, max_size):
     return [int(i == x) for i in range(max_size)]
@@ -74,9 +81,6 @@ if __name__ == '__main__':
                 "filename": sample
             })
         class_number += 1
-        if class_number >= 2:
-            break
-    
 
     neural_net = NeuralNetwork(100 * 100, len(labels))
 
@@ -86,18 +90,16 @@ if __name__ == '__main__':
     
     training_set = ((input_matrix), np.array([one_hot(x['output'], len(labels)) for x in training_set]))
     
-    neural_net.train(training_set, 0.5, 100)
+    neural_net.train(training_set, 0.5, 2000)
 
     classified = 0
     correctly_classified = 0
     classification_data = input_data[int(len(input_data) * 0.8):]
     for sample in classification_data:
         classified += 1
-        guess = neural_net.feed_forward(np.matrix(sample["input"]))
-        print(guess)
+        guess = neural_net.feed_forward(np.matrix(sample["input"]))[0]
         if guess == sample["output"]:
             correctly_classified += 1
-
 
     print("Úspěšnost %s " % str((correctly_classified / classified) * 100))
 
@@ -105,5 +107,7 @@ if __name__ == '__main__':
         indx = random.randint(0, len(input_data) - 1)
         fig = plt.imshow(np.matrix(input_data[indx]["input"]).reshape((100, 100)))
         plt.show()
-        j, k = (neural_net.feed_forward(np.matrix(input_data[indx]["input"])), input_data[indx]["output"])
+        j, k = (neural_net.feed_forward(np.matrix(input_data[indx]["input"]))[0], input_data[indx]["output"])
+        print(j, k)
         print("Neuronka si myslí, že vzorek je %s skutečnost je %s" % (labels[j], labels[k]))
+    neural_net.session.close()

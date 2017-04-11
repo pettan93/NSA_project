e10a7b51a6c5cb272bc4ecaf185009826e824b28
Pettan93 <panhammer@seznam.cz>
2017-04-11 12:06:43 +0200

    Cross-font generating, example two fotns dataset

--- captcha_processor/generator.py
+++ captcha_processor/generator.py
@@ -1,4 +1,6 @@
 import os
+import random
+import shutil
 import string
 from subprocess import check_output
 
@@ -12,7 +14,8 @@ def generate_captcha(text, output_path, settings_dict=None):
                      "rotationAmplitude",  # default 30
                      "scaleAmplitude",  # default 40
                      "showGrid",  # default False
-                     "gridSize"  # default 10
+                     "gridSize",  # default 10
+                     "font"
                      ]
 
     if settings_dict is None:
@@ -25,8 +28,9 @@ def generate_captcha(text, output_path, settings_dict=None):
         "java -jar "
         + RESOURCES_DIR_PATH + "captcha_generator.jar "
         + text + " "
-        + RESOURCES_DIR_PATH + "output/" + output_path + " "
-        + RESOURCES_DIR_PATH + "fonts/boxpot.ttf "
+        # + RESOURCES_DIR_PATH + "output/" + output_path + " "
+        + output_path + " "
+        + RESOURCES_DIR_PATH + "fonts/" + settings_dict["font"] + " "
         + settings_dict["height"] + " "
         + settings_dict["width"] + " "
         + settings_dict["fontSize"] + " "
@@ -38,21 +42,38 @@ def generate_captcha(text, output_path, settings_dict=None):
 
 
 def traning_data_alphabet(samples_per_letter, settings):
-    for index, char in enumerate(string.ascii_lowercase):
+    # for index, char in enumerate(string.ascii_lowercase):
+    for index, char in enumerate(("a", "b")):
         print("Generating letter '" + char + "' [" + str(index + 1) + "/" + str(len(string.ascii_lowercase)) + "]")
 
         cap_char = char.capitalize()
 
-        lowercase_dir = RESOURCES_DIR_PATH + "output/alphabet/lowercase/" + char
+        lowercase_dir = RESOURCES_DIR_PATH + "output/alphabet_2/lowercase/" + char
+        if os.path.exists(lowercase_dir):
+            shutil.rmtree(lowercase_dir)
         os.makedirs(lowercase_dir)
         for i in range(samples_per_letter):
             print(" - lowercase image [" + str(i + 1) + "/" + str(samples_per_letter) + "]")
+
+            if bool(random.getrandbits(1)):
+                settings["font"] = "pricedown.ttf"
+            else:
+                settings["font"] = "boxpot.ttf"
+
             generate_captcha(char, lowercase_dir + "/" + str(i + 1) + ".png", settings)
 
-        uppercase_dir = RESOURCES_DIR_PATH + "output/alphabet/uppercase/" + cap_char
+        uppercase_dir = RESOURCES_DIR_PATH + "output/alphabet_2/uppercase/" + cap_char
+        if os.path.exists(uppercase_dir):
+            shutil.rmtree(uppercase_dir)
         os.makedirs(uppercase_dir)
         for i in range(samples_per_letter):
             print(" - uppercase image [" + str(i + 1) + "/" + str(samples_per_letter) + "]")
+
+            if bool(random.getrandbits(1)):
+                settings["font"] = "pricedown.ttf"
+            else:
+                settings["font"] = "boxpot.ttf"
+
             generate_captcha(cap_char, uppercase_dir + "/" + str(i + 1) + ".png", settings)
 
 
@@ -62,4 +83,9 @@ def traning_data_alphabet(samples_per_letter, settings):
 
 
 # traning_data_alphabet(100, {"height": "100", "width": "100"})
-generate_captcha("bard", "out4.png")
+
+# generate_captcha("bard", "out1.png", {"font": "boxpot.ttf"})
+#
+# generate_captcha("bard", "out2.png", {"font": "pricedown.ttf"})
+
+traning_data_alphabet(100, {"height": "100", "width": "100", "font": "boxpot.ttf"})
